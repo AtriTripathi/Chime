b979e6d645190ad0e8c479fa71eaa8b71b344d77
AtriTripathi <tripathi.atri@gmail.com>
2021-02-03 00:35:47 +0530

    Finish MusicService implementation

--- app/src/main/java/com/atritripathi/chime/exoplayer/MusicService.kt
+++ app/src/main/java/com/atritripathi/chime/exoplayer/MusicService.kt
@@ -1,23 +1,25 @@
 package com.atritripathi.chime.exoplayer
 
 import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.atritripathi.chime.exoplayer.callbacks.MusicPlaybackPreparer
 import com.atritripathi.chime.exoplayer.callbacks.MusicPlayerEventListener
 import com.atritripathi.chime.exoplayer.callbacks.MusicPlayerNotificationListener
+import com.atritripathi.chime.others.Constants.MEDIA_ROOT_ID
 import com.atritripathi.chime.others.Constants.SERVICE_TAG
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -37,12 +39,26 @@ class MusicService : MediaBrowserServiceCompat() {
 
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
+    private lateinit var musicPlayerEventListener: MusicPlayerEventListener
 
     var isForegroundService = false
-    private var curPlayingSong: MediaMetadataCompat? = null
+
+    private var currentPlayingSong: MediaMetadataCompat? = null
+
+    private var isPlayerInitialized = false
+
+    companion object {
+        var currentSongDuration = 0L
+            private set
+    }
 
     override fun onCreate() {
         super.onCreate()
+
+        serviceScope.launch {
+            firebaseMusicSource.fetchMediaData()
+        }
+
         val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
             PendingIntent.getActivity(this, 0, it, 0)
         }
@@ -58,20 +74,28 @@ class MusicService : MediaBrowserServiceCompat() {
             mediaSession.sessionToken,
             MusicPlayerNotificationListener(this),
         ) { // This lambda block will be executed, whenever the song changes.
-
+            currentSongDuration = exoPlayer.duration
         }
 
         val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
-            curPlayingSong = it
-            preparePlayer(firebaseMusicSource.songs, curPlayingSong, true)
+            currentPlayingSong = it
+            preparePlayer(firebaseMusicSource.songs, currentPlayingSong, true)
         }
 
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlaybackPreparer(musicPlaybackPreparer)
+        mediaSessionConnector.setQueueNavigator(MusicQueueNavigator())
         mediaSessionConnector.setPlayer(exoPlayer)
 
-        exoPlayer.addListener(MusicPlayerEventListener(this))
+        musicPlayerEventListener = MusicPlayerEventListener(this)
+        exoPlayer.addListener(musicPlayerEventListener)
         musicNotificationManager.showNotification(exoPlayer)
+    }
+
+    private inner class MusicQueueNavigator : TimelineQueueNavigator(mediaSession) {
+        override fun getMediaDescription(player: Player, windowIndex: Int): MediaDescriptionCompat {
+            return firebaseMusicSource.songs[windowIndex].description
+        }
     }
 
     private fun preparePlayer(
@@ -79,7 +103,7 @@ class MusicService : MediaBrowserServiceCompat() {
         itemToPlay: MediaMetadataCompat?,
         playNow: Boolean
     ) {
-        val curSongIndex = if (curPlayingSong == null) 0 else songs.indexOf(itemToPlay)
+        val curSongIndex = if (currentPlayingSong == null) 0 else songs.indexOf(itemToPlay)
         exoPlayer.prepare(firebaseMusicSource.asMediaSource(dataSourceFactory))
         exoPlayer.seekTo(curSongIndex, 0L)
         exoPlayer.playWhenReady = playNow
@@ -90,17 +114,43 @@ class MusicService : MediaBrowserServiceCompat() {
         clientUid: Int,
         rootHints: Bundle?
     ): BrowserRoot? {
-
+        return BrowserRoot(MEDIA_ROOT_ID, null)
     }
 
     override fun onLoadChildren(
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
+        when(parentId) {
+            MEDIA_ROOT_ID -> {
+                val resultSent = firebaseMusicSource.whenReady { isSourceInitialized ->
+                    if (isSourceInitialized) {
+                        result.sendResult(firebaseMusicSource.asMediaItems())
+                        if (!isPlayerInitialized && firebaseMusicSource.songs.isNotEmpty()) {
+                            preparePlayer(firebaseMusicSource.songs, firebaseMusicSource.songs[0], false)
+                            isPlayerInitialized = true
+                        }
+                    } else {
+                        result.sendResult(null)
+                    }
+                }
+                if (!resultSent) {
+                    result.detach()
+                }
+            }
+        }
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        exoPlayer.stop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         serviceScope.cancel()
+        exoPlayer.removeListener(musicPlayerEventListener)
+        exoPlayer.release()
+
     }
 }
--- app/src/main/java/com/atritripathi/chime/others/Constants.kt
+++ app/src/main/java/com/atritripathi/chime/others/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
 
     const val NOTIFICATION_ID = 1
     const val NOTIFICATION_CHANNEL_ID = "music"
+
+    const val MEDIA_ROOT_ID = "media_root_id"
 }
